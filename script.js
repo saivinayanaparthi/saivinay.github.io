271113c2592f13e5a9acd7d3365ce13d74bcc8e8
saivinayanaparthi <saivinay.anaparth8@gmail.com>
2026-02-26 01:16:19 +0530

    doen for the day

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Portfolio Loaded");
+const elements=document.querySelectorAll(".fade");
+
+window.addEventListener("scroll",()=>{
+
+elements.forEach(el=>{
+
+if(el.getBoundingClientRect().top<window.innerHeight-100){
+
+el.classList.add("show");
+
+}
+
+});
+
+});
