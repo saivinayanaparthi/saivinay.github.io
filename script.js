930764c8e63bb59ac7f46f6ff0f61dee73e25dfe
saivinayanaparthi <saivinay.anaparth8@gmail.com>
2026-02-26 22:09:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,72 @@
-const elements=document.querySelectorAll(".fade");
+particlesJS("particles-js", {
 
-window.addEventListener("scroll",()=>{
+particles:{
 
-elements.forEach(el=>{
+number:{value:80},
 
-if(el.getBoundingClientRect().top<window.innerHeight-100){
+size:{value:3},
 
-el.classList.add("show");
+move:{speed:2}
 
 }
 
 });
 
-});
+
+function openProject(id){
+
+let title="";
+
+let desc="";
+
+
+if(id==1){
+
+title="Trail Card Inc";
+
+desc="Worked on Health Cloud, integrations, Apex, LWC, deployment, API integrations.";
+
+}
+
+
+if(id==2){
+
+title="Sobeys Inc";
+
+desc="Implemented Salesforce CPQ, pricing rules, quote templates, integrations.";
+
+}
+
+
+if(id==3){
+
+title="Waste Management";
+
+desc="Developed integrations, Apex, Visualforce, data model.";
+
+}
+
+
+if(id==4){
+
+title="Carrier Inc";
+
+desc="Built community cloud portal, integrations, SFCC backend.";
+
+}
+
+
+document.getElementById("modal").style.display="block";
+
+document.getElementById("modal-title").innerHTML=title;
+
+document.getElementById("modal-desc").innerHTML=desc;
+
+}
+
+
+function closeModal(){
+
+document.getElementById("modal").style.display="none";
+
+}
